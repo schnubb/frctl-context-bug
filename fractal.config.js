d3077babf3dddfa755c1aa7aa7c320e2017b9085
Petrakow, Andre (init) <andre.petrakow@init.de>
2020-06-28 21:58:08 +0200

    adds static build setup

--- fractal.config.js
+++ fractal.config.js
@@ -1,5 +1,8 @@
 "use strict";
+const path = require("path");
 
 const fractal = module.exports = require('@frctl/fractal').create();
 
 fractal.components.set('path', __dirname + '/components');
+
+fractal.web.set('builder.dest', path.join(__dirname, '/static'));
